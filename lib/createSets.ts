069df22fb30111f41e5f55c836391f9826572065
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2019-02-11 09:06:08 +0100

    chore(createSets): add options as last function parameter

--- lib/createSets.ts
+++ lib/createSets.ts
@@ -1,18 +1,24 @@
 import { createCombinations } from "./createCombinations";
+import { Options } from "./withIdentifiers/create";
 import { createCompleteCombinations } from "./withIdentifiers/createCompleteCombinations";
 
-export function createSets<Item>(collection: Item[]) {
+export function createSets<Item>(collection: Item[], options?: Options) {
   const collectionWithIdentifiers = collection.map((item, index) => ({
     item,
     index
   }));
-  const allCombinations = createCombinations(collectionWithIdentifiers);
-  const filteredCombinations = allCombinations.filter(
-    combination => combination.length > 0
-  );
+  const numberOfCalls = { calls: 0 };
+  const allCombinations = createCombinations(collectionWithIdentifiers, {
+    minimumLength: 1,
+    storeNumberOfCallsIn: numberOfCalls
+  });
   const sets = createCompleteCombinations<{ item: Item; index: number }[]>(
-    filteredCombinations,
-    item => item.map(i => i.index)
+    allCombinations,
+    item => item.map(i => i.index),
+    options
   );
+  if (options && options.storeNumberOfCallsIn) {
+    options.storeNumberOfCallsIn.calls += numberOfCalls.calls;
+  }
   return sets.map(set => set.map(coll => coll.map(({ item }) => item)));
 }
